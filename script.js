ec29cb62882681ef9308d9ed353625bf14659b8f
Rory Martin <rorymartin3d@gmail.com>
2023-05-17 23:36:26 +0100

    Grid colors on hover. Grid Size adjustable. Grid can be cleared

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const gridContainer = document.querySelector('.grid-container');
-const form = document.getElementById('gridSizeForm');
-const reset = document.getElementById('reset');
-//reset.addEventListener('click', resetColor);
+const form = document.getElementById('grid-size-form');
+const resetGrid = document.getElementById('reset');
+resetGrid.addEventListener('click', resetColorGrid);
 form.addEventListener('submit', changeSizeOfGrid);
 
 function createGridCells(gridSize) {
     // Set the grid container to display as a grid
     gridContainer.style.display = 'grid';
+
     // Set the number of columns and rows in the grid
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    // delete all cells before continuing so we don't add on top
+
+    // delete all cells before continuing so we don't add on top when resizing
     let squares = gridContainer.querySelectorAll('div');
     squares.forEach((div) => div.remove());
+
     // Create the grid cells
     for (let i = 0; i < gridSize * gridSize; i++) {
         let cell = document.createElement('div');
@@ -37,4 +40,11 @@ function changeSizeOfGrid(e) {
 function changeColor(e) {
     const cell = e.target;
     cell.classList.add('grid-cell-hover');
+}
+
+function resetColorGrid() {
+    const cells = document.querySelectorAll('.grid-cell');
+    cells.forEach((cell) => {
+        cell.classList.remove('grid-cell-hover');
+    });
 }
